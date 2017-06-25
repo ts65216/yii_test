60af681578a3000572cc76141289aa995a79071c
ts65216 <jian.wang@chinascope.com>
2017-06-25 23:00:55 +0800

    a

--- controllers/BookController.php
+++ controllers/BookController.php
@@ -10,13 +10,14 @@ class BookController extends Controller{
     public function actionBuy($id = 0){
         Yii::info('Book控制器 buy方法');
 
-        $get = Yii::$app->request->get();
-        echo '<br><br><br><br>';
-        var_dump($get);
+//        $response = Yii::$app->response;
+//        $response->format = \yii\web\Response::FORMAT_JSON;
+//
+//        $get = Yii::$app->request->get();
+//        echo '<br><br><br><br>';
+//        var_dump($get);
 
-        if($id==1) echo "id={$id}啊啊";
-        else if($id==2) echo "id={$id}啊啊";
-
+        echo "id={$id}啊啊";
 
         $book = new Book;
         $a = $book->buy();
